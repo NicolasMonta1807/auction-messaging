3789aac1bbc5ff41e3d20d6c9eeadb12002324c3
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-07-24 18:48:33 -0500

    fix environment variables loading

--- rest/producer/producer.py
+++ rest/producer/producer.py
@@ -7,31 +7,27 @@ load_dotenv()
 
 app = Flask(__name__)
 
-PORT = os.getenv("PORT")
-
-service_urls = {
-  "capture": os.getenv("CAPTURE_URL"),
-  "tracking": os.getenv("TRACKING_URL"),
-  "analytics": os.getenv("ANALYTICS_URL")
-}
+# Load Environment Variables
+PORT = os.getenv("PORT", "8080")
+CAPTURE = os.getenv("CAPTURE_URL", "http://bid-capture-service:5000/capture-bid")
+TRACKING = os.getenv("TRACKING_URL", "http://bid-tracking-service:5001/track-bid")
+ANALYTICS = os.getenv("ANALYTICS_URL", "http://bid-analytics-service:5002/analyse-bid")
 
 @app.route('/produce-bid', methods=['POST'])
 def produce_bid():
   bid_data = request.json
+
+  capture_response = requests.post(CAPTURE, json=bid_data)
   
-  # TODO: Modify bid data for each service
-  capture_response = requests.post(service_urls["capture"], json=bid_data)
+  tracking_response = requests.post(TRACKING, json=bid_data)
   
-  tracking_response = requests.post(service_urls["tracking"], json=bid_data)
-  
-  analytics_response = requests.post(service_urls["analytics"], json=bid_data)
+  analytics_response = requests.post(ANALYTICS, json=bid_data)
   
   return jsonify({
     "capture": capture_response.text,
     "tracking": tracking_response.text,
     "analytics": analytics_response.text
   }), 200
-  
   
 if __name__ == '__main__':
   app.run( port=PORT)
