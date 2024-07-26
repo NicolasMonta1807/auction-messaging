67bf0552eca98e8cdec6a9f60de5767d399b6d81
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-07-25 22:15:53 -0500

    fix: container naming conflicts

--- rest/run.sh
+++
@@ -1,7 +0,0 @@
-docker build -t bid-capture-service -f ./capture/dockerfile ./capture/
-docker build -t bid-tracking-service -f ./tracking/dockerfile ./tracking/
-docker build -t bid-analytics-service -f ./analytics/dockerfile ./analytics/
-
-docker build -t bid-producer-service -f ./producer/dockerfile ./producer/
-
-docker-compose up --build
